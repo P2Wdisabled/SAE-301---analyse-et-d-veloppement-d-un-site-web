e2e49ce0e666948f5f517b580d6d1278a9c02e42
Louis <louis1.potevin1@gmail.com>
2024-10-17 11:19:38 +0200

    reorganising folders

---
+++ iteration 2/api/Controller/ProductController.php
@@ -0,0 +1,72 @@
+<?php
+require_once "Controller.php";
+require_once "Repository/ProductRepository.php";
+
+/**
+ * Classe ProductController
+ * Gère les requêtes REST pour les produits.
+ */
+class ProductController extends Controller {
+
+    private ProductRepository $products;
+
+    public function __construct(){
+        $this->products = new ProductRepository();
+    }
+
+    protected function processGetRequest(HttpRequest $request) {
+        $id = $request->getId();
+
+        if ($id){
+            // URI est .../products/{id}
+            $p = $this->products->find($id);
+            return $p == null ? false : $p;
+        }
+        else{
+            // URI est .../products
+            return $this->products->findAll();
+        }
+    }
+
+    protected function processPostRequest(HttpRequest $request) {
+        $json = $request->getJson();
+        $obj = json_decode($json, true);
+        if (!$obj) return false;
+
+        $p = new Product(0); // 0 est une valeur temporaire
+        $p->setName($obj['name'] ?? '')
+          ->setDescription($obj['description'] ?? '')
+          ->setPrice(floatval($obj['price'] ?? 0))
+          ->setImageUrl($obj['image_url'] ?? '');
+        $ok = $this->products->save($p); 
+        return $ok ? $p : false;
+    }
+
+    protected function processDeleteRequest(HttpRequest $request) {
+        $id = $request->getId();
+        if ($id){
+            return $this->products->delete($id);
+        }
+        return false;
+    }
+
+    protected function processPutRequest(HttpRequest $request) {
+        $id = $request->getId();
+        if ($id){
+            $json = $request->getJson();
+            $obj = json_decode($json, true);
+            if (!$obj) return false;
+
+            $p = $this->products->find($id);
+            if ($p == null) return false;
+
+            $p->setName($obj['name'] ?? $p->getName())
+              ->setDescription($obj['description'] ?? $p->getDescription())
+              ->setPrice(floatval($obj['price'] ?? $p->getPrice()))
+              ->setImageUrl($obj['image_url'] ?? $p->getImageUrl());
+            return $this->products->update($p);
+        }
+        return false;
+    }
+}
+?>
--- no api/iteration 7/check_admin_auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['admin_id'])) {
-    echo json_encode(['isAdminAuthenticated' => true]);
-} else {
-    echo json_encode(['isAdminAuthenticated' => false]);
-}
-?>
